b22276c9ed070416135942a1c9d69c82ddd1d039
Gabriel <89980949+emptyvoxel@users.noreply.github.com>
2024-09-01 17:32:22 -0300

    Change package information

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `HyperVoxel`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Centro Acadêmico da Biologia USP`,
+    siteUrl: `https://sphncabio.github.io`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
